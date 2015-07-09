2c6776ac6d1f190b1161ee8320b7294d019f16a7
Reid Vandewiele <reid@puppetlabs.com>
2015-07-09 20:24:16 +0000

    Download JBoss installer

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,7 +1,10 @@
 class jboss (
-
+  $version = '7.1.1',
 ) {
 
-  notify { 'jboss class': }
+  remote_file { "C:/jboss-as-${version}.Final.zip":
+    ensure => present,
+    source => "http://download.jboss.org/jbossas/7.1/jboss-as-${version}.Final/jboss-as-${version}.Final.zip",
+  }
 
 }
