4299137ee511d7e10d3c67e181dd5c816c99be45
Reid Vandewiele <reid@puppetlabs.com>
2015-07-09 20:27:31 +0000

    unzip the installer

--- manifests/init.pp
+++ manifests/init.pp
@@ -5,6 +5,12 @@ class jboss (
   remote_file { "C:/jboss-as-${version}.Final.zip":
     ensure => present,
     source => "http://download.jboss.org/jbossas/7.1/jboss-as-${version}.Final/jboss-as-${version}.Final.zip",
+    before => Unzip['jboss_installer'],
+  }
+
+  unzip { "jboss_installer":
+    source  => "C:/jboss-as-${version}.Final.zip",
+    creates => "C:/jboss-as-${version}.Final",
   }
 
 }
